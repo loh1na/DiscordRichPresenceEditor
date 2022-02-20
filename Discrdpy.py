d3cfa1da1c710b8627b57972ad9fe1f4f7550ec0
loh1nich <70539994+loh1na@users.noreply.github.com>
2022-02-20 17:28:39 +0200

    Add files via upload

--- Discrdpy.py
+++ Discrdpy.py
@@ -2,6 +2,7 @@
 import sys, Settings
 from pypresence import *
 
+
 def Main():
 	print("Welcome to Discord Rich Presence editor\nplease paste a ClientID from your discord developer portal to Settings.py")
 	try:
@@ -9,15 +10,23 @@ def Main():
 			drp = Presence(Settings.Clientid)
 			print("starting and creating Rich Presence...")
 			drp.connect()
-			drp.update(large_text = Settings.large_text, small_text = Settings.small_text, large_image = Settings.image_large)
+			drp.update(state = Settings.large_text, details = Settings.small_text, large_image = Settings.image_large)
+			try:
+				if Settings.usebutton == True:
+					drp.update(buttons = [{"label":f"{Settings.buttontext}", "url":f"{Settings.buttonurl}"}])
+			except ServerError:
+				print(f"can't found {Settings.buttonurl}, try do add to begin of url https://")
 		
 	except DiscordError:
-		print("no ClientID found, aborting...")
+		print("\ncan't found ClientID, aborting...")
 		sys.exit()
 
 	except KeyboardInterrupt:
-		print("ctrl + c pressed, aborting...")
+		print("\nctrl + c pressed, aborting...")
 		sys.exit()
 
+	except DiscordNotFound:
+		print("\ncan't find discord on this pc, aborting...")
+		sys.exit()
 if __name__ == '__main__':
 	Main()
--- Settings.py
+++ Settings.py
@@ -1,9 +1,13 @@
 # here paste a ClientID
-Clientid = ''
+Clientid = '944848314728316948'
 # here is a default settings for rich presence
 
 #use image from developer portal assets
-image_large = ''
+image_large = 'lol'
 # here is a texts for rich presence 
-large_text = ''
-small_text = ''
+large_text = "test"
+small_text = "test"
+#set usebutton to true, to enable button
+usebutton = True
+buttontext = "test"
+buttonurl = "https://google.com"
